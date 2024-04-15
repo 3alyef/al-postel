620c347ae2991789624868644b11a796fb439019
3alyef <3alyef.dev@gmail.com>
2024-04-15 14:22:32 -0300

    correcao

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { i18n } from './i18n-config';
-import getLocale from './middlewares/getLocale.middleware';
+import getLocale from '@/middlewares/getLocale.middleware';
 
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
